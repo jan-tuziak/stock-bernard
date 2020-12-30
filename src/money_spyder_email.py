b8fb35db6d5244653b02d70e8a90e8d37c0f0c96
Draxgen <draxgen@gmail.com>
2020-12-30 15:00:22 +0100

    Email sent via Outlook

--- src/money_spyder_email.py
+++ src/money_spyder_email.py
@@ -8,23 +8,27 @@ class MoneySpyderEmail:
 
         msg = EmailMessage()
 
-        msg['Subject'] = "Money Spyder Lighthouse"
-        msg['From'] = 'money.spyder.2020@gmail.com'
+        msg['Subject'] = "Money Spyder's Lighthouse"
+        msg['From'] = 'money.spyder@outlook.com'
         msg['To'] = toAddress
 
-        msg.set_content('Money Spyder Lighthouse recommends these stocks to look at.')
+        msg.set_content("Money Spyder's Lighthouse recommends these stocks to look at.")
 
         msg.add_alternative(f"""
         <p>
-            <h1>Money Spyder Lighthouse</h1>
+            <h1>Money Spyder's Lighthouse</h1>
             Money Spyder Lighthouse recommends these stocks to look at:<br><br>
             <strong>{stocks_to_observe}</strong>
         </p>
         """, subtype='html')
 
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        with smtplib.SMTP('smtp-mail.outlook.com', 587) as smtp:
             smtp.ehlo()  # Say EHLO to server
             smtp.starttls(context=context)  # Puts the connection in TLS mode.
             smtp.ehlo()
-            smtp.login(msg['From'], "BlackJack88")
+            smtp.login(msg['From'], "MoneySpyder2020")
             smtp.send_message(msg)
+
+if __name__ == "__main__":    
+    msEmail = MoneySpyderEmail()
+    msEmail.SendLighthouseEmail('jan.tuziak@outlook.com', 'NYSE:AAPL')

190afe5075216d74d839ae338be2ba7df9af0b79
Jan Tuziak <jan.tuziak@gmail.com>
2022-02-14 16:55:36 +0100

    Working FastAPI on Windows

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def execute_inverted_lighthouse(sector=""):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="127.0.0.1", port=port)
